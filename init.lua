006a7eafc3786f983b055b3a499c6645d4da127f
kelvinst <kelvin.stinghen@me.com>
2024-11-14 15:01:25 -0300

    Adding Github Copilot

--- init.lua
+++ init.lua
@@ -728,6 +728,34 @@ require('lazy').setup({
     },
   },
 
+  { -- Github Copilot suggestions AI
+    'github/copilot.vim',
+    init = function()
+      vim.keymap.set('i', '<C-c><CR>', 'copilot#Accept("\\<CR>")', {
+        expr = true,
+        replace_keycodes = false,
+        desc = '[C]opilot Accept',
+      })
+
+      vim.keymap.set('i', '<C-c>n', '<Plug>(copilot-next)', {
+        noremap = false,
+        desc = '[C]opilot [N]ext',
+      })
+
+      vim.keymap.set('i', '<C-c>p', '<Plug>(copilot-previous)', {
+        noremap = false,
+        desc = '[C]opilot [P]revious',
+      })
+
+      vim.keymap.set('i', '<C-c><Space>', '<Plug>(copilot-suggest)', {
+        noremap = false,
+        desc = '[C]opilot Suggest',
+      })
+
+      vim.g.copilot_no_tab_map = true
+    end,
+  },
+
   { -- Autocompletion
     'hrsh7th/nvim-cmp',
     event = 'InsertEnter',

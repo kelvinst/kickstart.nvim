b91197ddb31643275135ecf12e5da8142a1abd10
kelvinst <kelvin.stinghen@me.com>
2024-11-16 20:06:37 -0300

    Changing lazygit to neogit

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -3,7 +3,16 @@
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-  local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
+
+  local out = vim.fn.system {
+    'git',
+    'clone',
+    '--filter=blob:none',
+    '--branch=stable',
+    lazyrepo,
+    lazypath,
+  }
+
   if vim.v.shell_error ~= 0 then
     error('Error cloning lazy.nvim:\n' .. out)
   end
@@ -36,7 +45,7 @@ require('lazy').setup({
   require 'plugins.neo-tree', -- Better file tree
   require 'plugins.undotree', -- Better undo history
   require 'plugins.gitsigns', -- Adds gitsigns recommend keymaps
-  require 'plugins.lazygit', -- Git UI in Neovim
+  require 'plugins.neogit', -- Git UI in Neovim
   -- require 'plugins.debug', -- Debugging
   -- require 'plugins.lint', -- Linting
   -- require 'plugins.autopairs', -- Autopairs

35fe68f4318f53578990b6b39d23fc413178f376
kelvinst <kelvin.stinghen@me.com>
2024-11-20 18:29:25 -0300

    Breaking lines at 80

--- lua/plugins/vim-tmux-navigator.lua
+++ lua/plugins/vim-tmux-navigator.lua
@@ -8,10 +8,30 @@ return {
     'TmuxNavigatePrevious',
   },
   keys = {
-    { '<c-h>', '<cmd><C-U>TmuxNavigateLeft<cr>', desc = 'Go to the left window' },
-    { '<c-j>', '<cmd><C-U>TmuxNavigateDown<cr>', desc = 'Go to the down window' },
-    { '<c-k>', '<cmd><C-U>TmuxNavigateUp<cr>', desc = 'Go to the up window' },
-    { '<c-l>', '<cmd><C-U>TmuxNavigateRight<cr>', desc = 'Go to the right window' },
-    { '<c-\\>', '<cmd><C-U>TmuxNavigatePrevious<cr>', desc = 'Go to previous window' },
+    {
+      '<c-h>',
+      '<cmd><C-U>TmuxNavigateLeft<cr>',
+      desc = 'Go to the left window',
+    },
+    {
+      '<c-j>',
+      '<cmd><C-U>TmuxNavigateDown<cr>',
+      desc = 'Go to the down window',
+    },
+    {
+      '<c-k>',
+      '<cmd><C-U>TmuxNavigateUp<cr>',
+      desc = 'Go to the up window',
+    },
+    {
+      '<c-l>',
+      '<cmd><C-U>TmuxNavigateRight<cr>',
+      desc = 'Go to the right window',
+    },
+    {
+      '<c-\\>',
+      '<cmd><C-U>TmuxNavigatePrevious<cr>',
+      desc = 'Go to previous window',
+    },
   },
 }
